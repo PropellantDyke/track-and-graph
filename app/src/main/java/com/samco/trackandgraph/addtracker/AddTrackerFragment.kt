37ed7f2329ff6ebd26aae596066270dcf1707455
Propellant Dyke <propellant.dyke@gmail.com>
2023-12-11 22:26:20 +0100

    WIP

--- app/src/main/java/com/samco/trackandgraph/addtracker/AddTrackerFragment.kt
+++ app/src/main/java/com/samco/trackandgraph/addtracker/AddTrackerFragment.kt
@@ -20,48 +20,102 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.ui.platform.ComposeView
+import android.widget.Button
+import androidx.databinding.Observable
+import androidx.databinding.ObservableBoolean
+import androidx.databinding.ObservableField
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.button.MaterialButtonToggleGroup
 import com.samco.trackandgraph.MainActivity
 import com.samco.trackandgraph.NavButtonStyle
 import com.samco.trackandgraph.R
-import com.samco.trackandgraph.ui.compose.theming.TnGComposeTheme
-import com.samco.trackandgraph.util.*
+import com.samco.trackandgraph.base.database.dto.DataType
+import com.samco.trackandgraph.base.database.dto.TrackerSuggestionOrder
+import com.samco.trackandgraph.base.database.dto.TrackerSuggestionType
+import com.samco.trackandgraph.databinding.AddTrackerBinding
+import com.samco.trackandgraph.util.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @AndroidEntryPoint
 class AddTrackerFragment : Fragment() {
     private val args: AddTrackerFragmentArgs by navArgs()
-    private val viewModel by viewModels<AddTrackerViewModelImpl>()
+    private val viewModel: AddTrackerViewModel by viewModels()
+    private val uiData = UiData()
     private var navController: NavController? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel.init(args.groupId, args.editFeatureId)
+    class UiData {
+        val isNewTracker = ObservableBoolean(true)
+
+        val defaultSection = ObservableBoolean(false)
+        val suggestionSection = ObservableBoolean(false)
+
+        val errorNameEmpty = ObservableBoolean(false)
+        val errorNameTaken = ObservableBoolean(false)
+        val errorInternal = ObservableBoolean(false)
+
+        var trackerName = ObservableField<String>()
+        var trackerDescription = ObservableField<String>()
+        var dataType = ObservableField<Int>()
+        var defaultValue = ObservableField<String>()
+        var defaultLabel = ObservableField<String>()
+        var suggestionType = ObservableField<Int>()
+        var suggestionOrder = ObservableField<Int>()
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
+        uiData.isNewTracker.set(args.editFeatureId == -1L)
+
+        val binding = AddTrackerBinding.inflate(inflater, container, false)
+        binding.uiData = uiData
+
         navController = container?.findNavController()
-        return ComposeView(requireContext()).apply {
-            setContent {
-                TnGComposeTheme {
-                    AddTrackerView(viewModel)
-                }
+
+        val view = binding.root
+
+        // Configure action button
+        view.findViewById<View>(R.id.actionButton).setOnClickListener { submitForm() }
+
+        view.findViewById<View>(R.id.closeButton).setOnClickListener { navController?.popBackStack() }
+
+        configureButtonToggleGroup(view, R.id.inputDataType)
+
+        // Handle errors from view model
+        viewModel.errors.observe(viewLifecycleOwner) { errors ->
+            errors.map {
+                uiData.errorNameEmpty.set(errors.contains(AddTrackerViewModel.TrackerEditionError.NAME_EMPTY))
+                uiData.errorNameTaken.set(errors.contains(AddTrackerViewModel.TrackerEditionError.NAME_TAKEN))
+                uiData.errorInternal.set(errors.contains(AddTrackerViewModel.TrackerEditionError.INTERNAL))
             }
         }
+
+        // Remove errors related to name when the name changed
+        uiData.trackerName.addOnPropertyChangedCallback(object :
+            Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(observable: Observable?, propertyId: Int) {
+                if (uiData.trackerName.get()?.isNotBlank() == true)
+                {
+                    uiData.errorNameEmpty.set(false)
+                    uiData.errorNameTaken.set(false)
+                }
+            }
+        })
+
+        fillForm(view)
+
+        return view
     }
 
     override fun onStart() {
         super.onStart()
-        viewModel.complete.observe(viewLifecycleOwner) {
+        viewModel.success.observe(viewLifecycleOwner) {
             if (it) navController?.popBackStack()
         }
     }
@@ -77,5 +131,69 @@ class AddTrackerFragment : Fragment() {
     override fun onStop() {
         super.onStop()
         requireActivity().window.hideKeyboard(requireActivity().currentFocus?.windowToken)
+    }
+
+    private fun configureButtonToggleGroup(view: View, toggleId: Int) {
+        val toggleGroup = view.findViewById<MaterialButtonToggleGroup>(toggleId)
+        toggleGroup.addOnButtonCheckedListener { _, buttonId, isChecked ->
+            if (isChecked) {
+                uiData.dataType.set(buttonId)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun fillForm(view: View) {
+        val deferredFormData = viewModel.getInitialValues(args.editFeatureId)
+        deferredFormData.invokeOnCompletion {
+            val formInput = deferredFormData.getCompleted()
+            uiData.trackerName.set(formInput.name)
+            uiData.trackerDescription.set(formInput.description)
+
+            view.findViewById<MaterialButtonToggleGroup>(R.id.inputDataType).check(when (formInput.dataType) {
+                DataType.CONTINUOUS -> R.id.dataTypeContinuous
+                DataType.DURATION -> R.id.dataTypeDuration
+            })
+
+            uiData.defaultSection.set(formInput.hasDefaultValue)
+            uiData.defaultValue.set(formInput.defaultValue.toString())
+            uiData.defaultLabel.set(formInput.defaultLabel)
+            uiData.trackerDescription.set(formInput.description)
+            uiData.suggestionType.set(formInput.suggestionType.ordinal)
+            uiData.suggestionOrder.set(formInput.suggestionOrder.ordinal)
+
+
+        }
+    }
+
+    private fun submitForm() {
+        val formInput = AddTrackerViewModel.TrackerData()
+
+        formInput.trackerId = args.editFeatureId
+        formInput.groupId = args.groupId
+        uiData.trackerName.get()?.let { formInput.name = it.trim() }
+        uiData.trackerDescription.get()?.let { formInput.description = it.trim() }
+
+        uiData.dataType.get()?.let {
+            formInput.dataType = when (it) {
+                R.id.dataTypeContinuous -> DataType.CONTINUOUS
+                R.id.dataTypeDuration -> DataType.DURATION
+                else -> DataType.CONTINUOUS
+            }
+        }
+
+        if (uiData.defaultSection.get()) {
+            formInput.hasDefaultValue = true
+            uiData.defaultValue.get()?.let { formInput.defaultValue = it.toDouble() }
+            uiData.defaultLabel.get()?.let { formInput.defaultLabel = it.trim() }
+        } else {
+            formInput.hasDefaultValue = false
+            formInput.defaultValue = 0.0
+            formInput.defaultLabel = ""
+        }
+        uiData.suggestionType.get()?.let { formInput.suggestionType = TrackerSuggestionType.values()[it] }
+        uiData.suggestionOrder.get()?.let { formInput.suggestionOrder = TrackerSuggestionOrder.values()[it] }
+
+        viewModel.processForm(formInput)
     }
 }
--- app/src/main/java/com/samco/trackandgraph/addtracker/AddTrackerViewModel.kt
+++ app/src/main/java/com/samco/trackandgraph/addtracker/AddTrackerViewModel.kt
@@ -1,268 +1,135 @@
 package com.samco.trackandgraph.addtracker
 
-import androidx.compose.runtime.*
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.*
-import com.samco.trackandgraph.R
 import com.samco.trackandgraph.base.database.dto.DataType
 import com.samco.trackandgraph.base.database.dto.Tracker
 import com.samco.trackandgraph.base.database.dto.TrackerSuggestionOrder
 import com.samco.trackandgraph.base.database.dto.TrackerSuggestionType
 import com.samco.trackandgraph.base.model.DataInteractor
-import com.samco.trackandgraph.base.model.TrackerHelper
 import com.samco.trackandgraph.base.model.di.IODispatcher
-import com.samco.trackandgraph.base.model.di.MainDispatcher
-import com.samco.trackandgraph.ui.viewmodels.DurationInputViewModel
-import com.samco.trackandgraph.ui.viewmodels.DurationInputViewModelImpl
-import com.samco.trackandgraph.ui.viewmodels.asValidatedDouble
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-interface AddTrackerViewModel : DurationInputViewModel {
-    //Outputs
-    val trackerName: TextFieldValue
-    val trackerDescription: TextFieldValue
-    val isDuration: LiveData<Boolean>
-    val isLoading: LiveData<Boolean>
-    val hasDefaultValue: LiveData<Boolean>
-    val defaultValue: TextFieldValue
-    val defaultLabel: TextFieldValue
-    val createButtonEnabled: LiveData<Boolean>
-    val errorText: LiveData<Int?>
-    val durationNumericConversionMode: LiveData<TrackerHelper.DurationNumericConversionMode>
-    val shouldShowDurationConversionModeSpinner: LiveData<Boolean>
-    val isUpdateMode: LiveData<Boolean>
-    val showUpdateWarningAlertDialog: LiveData<Boolean>
-    val suggestionType: LiveData<TrackerSuggestionType>
-    val suggestionOrder: LiveData<TrackerSuggestionOrder>
-
-    //Inputs
-    fun onTrackerNameChanged(name: TextFieldValue)
-    fun onTrackerDescriptionChanged(description: TextFieldValue)
-    fun onIsDurationCheckChanged(isDuration: Boolean)
-    fun onHasDefaultValueChanged(hasDefaultValue: Boolean)
-    fun onDefaultValueChanged(defaultValue: TextFieldValue)
-    fun onDefaultLabelChanged(defaultLabel: TextFieldValue)
-    fun onDurationNumericConversionModeChanged(durationNumericConversionMode: TrackerHelper.DurationNumericConversionMode)
-    fun onConfirmUpdate()
-    fun onDismissUpdateWarningCancel()
-    fun onCreateUpdateClicked()
-    fun onSuggestionTypeChanged(suggestionType: TrackerSuggestionType)
-    fun onSuggestionOrderChanged(suggestionOrder: TrackerSuggestionOrder)
-}
-
-//TODO so much mutable state :/ ugly
 @HiltViewModel
-class AddTrackerViewModelImpl @Inject constructor(
+class AddTrackerViewModel @Inject constructor(
     private val dataInteractor: DataInteractor,
-    @IODispatcher private val io: CoroutineDispatcher,
-    @MainDispatcher private val ui: CoroutineDispatcher,
-) : ViewModel(), AddTrackerViewModel, DurationInputViewModel by DurationInputViewModelImpl() {
+    @IODispatcher private val io: CoroutineDispatcher
+): ViewModel() {
 
-    private var disallowedNames: List<String>? = null
-
-    private sealed interface ValidationError {
-        object NoName : ValidationError
-        object NameAlreadyExists : ValidationError
+    enum class TrackerEditionError {
+        NAME_EMPTY,
+        NAME_TAKEN,
+        INTERNAL
     }
 
-    private val isUpdateModeFlow = MutableStateFlow(false)
-    private val isDurationModeFlow = MutableStateFlow(false)
-
-    override var trackerName by mutableStateOf(TextFieldValue(""))
-    override var trackerDescription by mutableStateOf(TextFieldValue(""))
-    override val isLoading = MutableLiveData(false)
-    override val hasDefaultValue = MutableLiveData(false)
-    override var defaultValue by mutableStateOf(TextFieldValue("1.0", TextRange(0, 3)))
-    override var defaultLabel by mutableStateOf(TextFieldValue())
-    override val isDuration = isDurationModeFlow
-        .asLiveData(viewModelScope.coroutineContext)
-    override val isUpdateMode: LiveData<Boolean> =
-        isUpdateModeFlow.asLiveData(viewModelScope.coroutineContext)
-    override val showUpdateWarningAlertDialog = MutableLiveData(false)
-
-    override val suggestionType = MutableLiveData(TrackerSuggestionType.LABEL_ONLY)
-    override val suggestionOrder = MutableLiveData(TrackerSuggestionOrder.LABEL_ASCENDING)
-
-    override fun onTrackerNameChanged(name: TextFieldValue) {
-        trackerName = name
+    class TrackerData {
+        var trackerId: Long = -1L
+        var groupId: Long = -1L
+        var name: String = ""
+        var description: String = ""
+        var type: DataType = DataType.CONTINUOUS
+        var hasDefaultValue: Boolean = false
+        var defaultValue: Double = 0.0
+        var defaultLabel: String = ""
+        var dataType: DataType = DataType.CONTINUOUS
+        var suggestionType: TrackerSuggestionType = TrackerSuggestionType.VALUE_AND_LABEL
+        var suggestionOrder: TrackerSuggestionOrder = TrackerSuggestionOrder.VALUE_ASCENDING
     }
 
-    override fun onTrackerDescriptionChanged(description: TextFieldValue) {
-        trackerDescription = description
-    }
+    val success = MutableLiveData<Boolean>(false)
+    val errors = MutableLiveData<Set<TrackerEditionError>>()
 
-    private val validationErrorFlow = snapshotFlow { trackerName }
-        .map {
-            when {
-                it.text.isBlank() -> ValidationError.NoName
-                disallowedNames?.contains(it.text) == true -> ValidationError.NameAlreadyExists
-                else -> null
-            }
-        }
+    fun getInitialValues(trackerId: Long): CompletableDeferred<TrackerData> {
+        val deferredFormInput = CompletableDeferred<TrackerData>()
 
-    override val createButtonEnabled = validationErrorFlow
-        .map { it == null }
-        .asLiveData(viewModelScope.coroutineContext)
-    override val errorText = validationErrorFlow
-        .map {
-            when (it) {
-                ValidationError.NoName -> R.string.tracker_name_cannot_be_null
-                ValidationError.NameAlreadyExists -> R.string.tracker_with_that_name_exists
-                else -> null
-            }
-        }
-        .asLiveData(viewModelScope.coroutineContext)
-    override val durationNumericConversionMode =
-        MutableLiveData(TrackerHelper.DurationNumericConversionMode.HOURS)
-    override val shouldShowDurationConversionModeSpinner: LiveData<Boolean> =
-        combine(isUpdateModeFlow, isDurationModeFlow) { a, b ->
-            //If we're in update mode and we've changed to or from duration we need to show
-            // a conversion mode spinner
-            a && (existingTracker?.dataType == DataType.DURATION) != b
-        }.asLiveData(viewModelScope.coroutineContext)
-    val complete = MutableLiveData(false)
-
-    private var groupId: Long = -1
-    private var existingTracker: Tracker? = null
-    private var initialized = false
-
-    fun init(groupId: Long, existingTrackerId: Long) {
-        if (initialized) return
-        initialized = true
-
-        this.groupId = groupId
         viewModelScope.launch(io) {
-            withContext(ui) { isLoading.value = true }
-            dataInteractor.getTrackerById(existingTrackerId)?.let {
-                initFromTracker(it)
+            dataInteractor.getTrackerById(trackerId)?.let {
+                val trackerData = TrackerData()
+                trackerData.name = it.name
+                trackerData.description = it.description
+                trackerData.dataType = it.dataType
+                trackerData.hasDefaultValue = it.hasDefaultValue
+                trackerData.defaultValue = it.defaultValue
+                trackerData.defaultLabel = it.defaultLabel
+                trackerData.suggestionType = it.suggestionType
+                trackerData.suggestionOrder = it.suggestionOrder
+                deferredFormInput.complete(trackerData)
             }
-            disallowedNames = dataInteractor
-                .getFeaturesForGroupSync(groupId)
-                .map { it.name }
-                .filter { it != existingTracker?.name }
-            withContext(ui) { isLoading.value = false }
         }
+
+        return deferredFormInput
     }
 
-    private suspend fun initFromTracker(tracker: Tracker) = withContext(ui) {
-        existingTracker = tracker
-        trackerName = TextFieldValue(tracker.name, TextRange(tracker.name.length))
-        trackerDescription = TextFieldValue(tracker.description, TextRange(tracker.name.length))
-        isDurationModeFlow.value = tracker.dataType == DataType.DURATION
-        hasDefaultValue.value = tracker.hasDefaultValue
+    fun processForm(formInput: TrackerData) {
+        var currentStatus = false
+        val currentErrors = mutableSetOf<TrackerEditionError>()
 
-        val defaultValueStr = tracker.defaultValue.toString()
-        defaultValue = TextFieldValue(defaultValueStr, TextRange(defaultValueStr.length))
-        defaultLabel = TextFieldValue(tracker.defaultLabel, TextRange(tracker.defaultLabel.length))
-        suggestionType.value = tracker.suggestionType
-        suggestionOrder.value = tracker.suggestionOrder
-        isUpdateModeFlow.value = true
-    }
-
-    override fun onIsDurationCheckChanged(isDuration: Boolean) {
-        this.isDurationModeFlow.value = isDuration
-    }
-
-    override fun onHasDefaultValueChanged(hasDefaultValue: Boolean) {
-        this.hasDefaultValue.value = hasDefaultValue
-    }
-
-    override fun onDefaultValueChanged(defaultValue: TextFieldValue) {
-        this.defaultValue = defaultValue.copy(text = defaultValue.text.asValidatedDouble())
-    }
-
-    override fun onDefaultLabelChanged(defaultLabel: TextFieldValue) {
-        this.defaultLabel = defaultLabel
-    }
-
-    override fun onDurationNumericConversionModeChanged(durationNumericConversionMode: TrackerHelper.DurationNumericConversionMode) {
-        this.durationNumericConversionMode.value = durationNumericConversionMode
-    }
-
-    override fun onConfirmUpdate() {
-        showUpdateWarningAlertDialog.value = false
         viewModelScope.launch(io) {
-            withContext(ui) { isLoading.value = true }
-            //should not be null here but just in case lets fallback to add
-            existingTracker?.let { updateTracker(it) } ?: addTracker()
-            withContext(ui) { isLoading.value = false }
-            withContext(ui) { complete.value = true }
-        }
-    }
-
-    override fun onDismissUpdateWarningCancel() {
-        showUpdateWarningAlertDialog.value = false
-    }
-
-    override fun onCreateUpdateClicked() {
-        if (isUpdateModeFlow.value) {
-            showUpdateWarningAlertDialog.value = true
-        } else {
-            viewModelScope.launch(io) {
-                withContext(ui) { isLoading.value = true }
-                addTracker()
-                withContext(ui) { isLoading.value = false }
-                withContext(ui) { complete.value = true }
+            if (formInput.name.isBlank()) {
+                currentErrors.add(TrackerEditionError.NAME_EMPTY)
             }
+
+            if (currentErrors.isEmpty()) {
+                var existingTracker: Tracker? = null
+
+                var disallowedNames = dataInteractor.getFeaturesForGroupSync(formInput.groupId)
+                    .map { it.name.trim() }
+
+                if (formInput.trackerId > 0) {
+                    existingTracker = dataInteractor.getTrackerById(formInput.trackerId)
+
+                    if (existingTracker != null) {
+                        disallowedNames = disallowedNames.filter { it != existingTracker.name }
+                    } else {
+                        currentErrors.add(TrackerEditionError.INTERNAL)
+                    }
+                }
+
+                if (disallowedNames.contains(formInput.name)) {
+                    currentErrors.add(TrackerEditionError.NAME_TAKEN)
+                }
+
+                // Everything is fine, go for update
+                if (currentErrors.isEmpty()) {
+                    if (formInput.trackerId > 0 && existingTracker != null) {
+                        dataInteractor.updateTracker(
+                            oldTracker = existingTracker,
+                            newName = formInput.name,
+                            featureDescription = formInput.description,
+                            newType = formInput.dataType,
+                            hasDefaultValue = formInput.hasDefaultValue,
+                            defaultValue = formInput.defaultValue,
+                            defaultLabel = formInput.defaultLabel,
+                            suggestionType = formInput.suggestionType,
+                            suggestionOrder = formInput.suggestionOrder
+                        )
+                    } else {
+                        val tracker = Tracker(
+                            id = 0L,
+                            name = formInput.name,
+                            groupId = formInput.groupId,
+                            featureId = 0L,
+                            displayIndex = 0,
+                            description = formInput.description,
+                            dataType = formInput.dataType,
+                            hasDefaultValue = formInput.hasDefaultValue,
+                            defaultValue = formInput.defaultValue,
+                            defaultLabel = formInput.defaultLabel,
+                            suggestionType = formInput.suggestionType,
+                            suggestionOrder = formInput.suggestionOrder
+                        )
+                        dataInteractor.insertTracker(tracker)
+                    }
+
+                    currentStatus = true
+                }
+            }
+
+            errors.postValue(currentErrors)
+            success.postValue(currentStatus)
         }
-    }
-
-    override fun onSuggestionTypeChanged(suggestionType: TrackerSuggestionType) {
-        this.suggestionType.value = suggestionType
-    }
-
-    override fun onSuggestionOrderChanged(suggestionOrder: TrackerSuggestionOrder) {
-        this.suggestionOrder.value = suggestionOrder
-    }
-
-    private fun getDataType() = when (isDuration.value) {
-        true -> DataType.DURATION
-        else -> DataType.CONTINUOUS
-    }
-
-    private fun getDefaultValue() = when (isDuration.value) {
-        true -> getDurationAsDouble()
-        else -> defaultValue.text.toDoubleOrNull()
-    }
-
-    private suspend fun updateTracker(existingTracker: Tracker) {
-        dataInteractor.updateTracker(
-            oldTracker = existingTracker,
-            durationNumericConversionMode = durationNumericConversionMode.value,
-            newName = trackerName.text,
-            newType = getDataType(),
-            hasDefaultValue = hasDefaultValue.value,
-            defaultValue = getDefaultValue() ?: 1.0,
-            featureDescription = trackerDescription.text,
-            defaultLabel = defaultLabel.text,
-            suggestionType = suggestionType.value,
-            suggestionOrder = suggestionOrder.value
-        )
-    }
-
-    private suspend fun addTracker() {
-        val tracker = Tracker(
-            id = 0L,
-            name = trackerName.text,
-            groupId = groupId,
-            featureId = 0L,
-            displayIndex = 0,
-            description = trackerDescription.text,
-            dataType = getDataType(),
-            hasDefaultValue = hasDefaultValue.value ?: false,
-            defaultValue = getDefaultValue() ?: 1.0,
-            defaultLabel = defaultLabel.text,
-            suggestionType = suggestionType.value ?: TrackerSuggestionType.VALUE_AND_LABEL,
-            suggestionOrder = suggestionOrder.value ?: TrackerSuggestionOrder.VALUE_ASCENDING
-        )
-        dataInteractor.insertTracker(tracker)
     }
 }
